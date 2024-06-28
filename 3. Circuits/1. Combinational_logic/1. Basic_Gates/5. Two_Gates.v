1921cac5afd4c8f13dd60c320534e5402a0f2648
TEEDA HEMANTH KUMAR <97118214+HemanthK-Git@users.noreply.github.com>
2024-06-28 11:25:11 +0530

    Create 5. Two_Gates.v

---
+++ 3. Circuits/1. Combinational_logic/1. Basic_Gates/5. Two_Gates.v
@@ -0,0 +1,13 @@
+// implement the gate
+
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+    wire w1;
+    
+    assign w1=~(in1^in2);
+    assign out=in3^w1;
+
+endmodule
