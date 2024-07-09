ef0688ec5236aba794a7f562c4cda737b70c75d6
TEEDA HEMANTH KUMAR <97118214+HemanthK-Git@users.noreply.github.com>
2024-07-09 20:02:05 +0530

    Update 1. D-flipflop.v

--- 3. Circuits/2. Sequential_logic/1. Latches_and_flipflops/1. D-flipflop.v
+++ 3. Circuits/2. Sequential_logic/1. Latches_and_flipflops/1. D-flipflop.v
@@ -1,1 +1,12 @@
-dfdf
+/* A D flip-flop is a circuit that stores a bit and is updated periodically, at the (usually) positive edge of a clock signal.
+D flip-flops are created by the logic synthesizer when a clocked always block is used (See alwaysblock2).
+A D flip-flop is the simplest form of "blob of combinational logic followed by a flip-flop" where the combinational logic portion is just a wire. */
+
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );
+    
+  always @(posedge clk) q=d;
+
+endmodule
