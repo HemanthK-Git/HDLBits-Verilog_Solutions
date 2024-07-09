c92ad86ef45de46b7a139e4fc24bcaa432acd1da
TEEDA HEMANTH KUMAR <97118214+HemanthK-Git@users.noreply.github.com>
2024-07-09 12:43:28 +0530

    Create 2. 4-variables.v

---
+++ 3. Circuits/1. Combinational_logic/4. Karnaugh_map_to-circuit/2. 4-variables.v
@@ -0,0 +1,12 @@
+// Implement the circuit described by the Karnaugh map below.
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    
+    assign out=((~a)*b*c)|(a*c*d)|((~a)*(~d))|((~c)*(~b));
+
+endmodule
