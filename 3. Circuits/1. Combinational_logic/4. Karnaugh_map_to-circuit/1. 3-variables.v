3c7acfa3f0d06abd28c54b5ec238424c290217cb
TEEDA HEMANTH KUMAR <97118214+HemanthK-Git@users.noreply.github.com>
2024-07-09 12:38:21 +0530

    Create 1. 3-variables.v

---
+++ 3. Circuits/1. Combinational_logic/4. Karnaugh_map_to-circuit/1. 3-variables.v
@@ -0,0 +1,10 @@
+// Implement the circuit described by the Karnaugh map below.
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    output out  ); 
+    assign out=a|b|c;
+
+endmodule
